70fbb0060944f72f3b159f7807e1e4fe4f5eca0f
toki513 <tokitahmidkhan@gmail.com>
2025-01-23 02:16:34 +0600

    updated

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Post
 
+
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'posts/posts_list.html',{'posts' :posts })
+
+
+def post_page(request,slug):
+    post = Post.objects.get(slug = slug)
+    return render(request, 'posts/post_page.html' ,{'post' : post})
