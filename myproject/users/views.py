98ca5805bdfcd62eb3b4258b7e4880469b04891d
toki513 <tokitahmidkhan@gmail.com>
2025-01-24 10:08:21 +0600

    updated

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def register_now(request):
-    return render(request,"users/register.html")
+    if request.method == "POST": 
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:list")
+    else:
+        form = UserCreationForm()
+    return render(request,"users/register.html", {"form" : form})
+
+
+
